91de83dcb3fdaf46d4ab73610285671a19d03d8c
KaDerox <derozja@gmail.com>
2026-03-30 21:00:51 +0200

    Add biome spawn registration and retrieval functions

--- AxoWorldGen.h
+++ AxoWorldGen.h
@@ -1,9 +1,16 @@
 #pragma once
+#include <vector>
+#include <string>
 
 class Level;
 class Random;
 class Biome;
 struct AxoBlockSpawnDef;
+struct AxoBiomeDef;
 
 void AxoWorldGen_RegisterSpawn(int blockId, const AxoBlockSpawnDef& spawn);
 void AxoWorldGen_Decorate(Level* level, Random* random, Biome* biome, int xo, int zo);
+
+struct AxoBiomeSpawnEntry { int biomeId; int weight; };
+void AxoWorldGen_RegisterBiomeSpawn(int biomeId, int weight);
+const std::vector<AxoBiomeSpawnEntry>& AxoWorldGen_GetCustomBiomes();
