5dc0e46e293df862c950512bda80c701b5c4867b
KaDerox <derozja@gmail.com>
2026-03-30 20:59:49 +0200

    Enhance AxoModLoader with terrain icon management
    Added functions for caching and retrieving terrain icons, as well as additional API flush functions.

--- AxoModLoader.h
+++ AxoModLoader.h
@@ -2,12 +2,17 @@
 
 #include <string>
 
+class Icon;
+class IconRegister;
+
 void AxoModLoader_PreInit(const char* modsDir);
 void AxoModLoader_MidInit();
 bool AxoModLoader_IsTerrainIconQueued(const std::wstring& name);
 void AxoModLoader_Init(const char* modsDir);
 void AxoModLoader_Tick();
 void AxoModLoader_Shutdown();
+void AxoModLoader_CacheTerrainIcon(const std::wstring& name, Icon* icon);
+Icon* AxoModLoader_GetTerrainIcon(const std::wstring& name);
 
 struct AxoAPITable;
 AxoAPITable* AxoAPI_GetTable();
@@ -18,4 +23,8 @@ void         AxoAPI_FlushCreativeMenu();
 struct AxoRecipeDef;
 bool AxoRecipe_CreateFromDef(const AxoRecipeDef& def);
 void AxoAPI_FlushRecipeRegistrations();
+void AxoAPI_FlushBiomeRegistrations();
+void AxoAPI_FlushCropRegistrations();
+void AxoAPI_RegisterCropSeedForCreative(int seedItemId, int creativeTab);
 int  AxoAPI_ResolveItemName(const std::string& name);
+int  AxoAPI_ResolveBlockName(const std::string& name);
